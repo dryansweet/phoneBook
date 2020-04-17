778bce6a2d48d7d9cdfbc87ebd7ee0aad27b2a08
D Ryan Sweet <ryansweet@northwesternmutual.com>
2020-04-17 14:24:47 -0500

    Adding frontend

---
+++ build/precache-manifest.3f5b180d146ef74d56da85bbcb7e86a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6571a466ac2edb6b48da2c05c8e6b01d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a2b257c23861fe0fae6",
+    "url": "/static/css/main.c86489f8.chunk.css"
+  },
+  {
+    "revision": "70ffa00f1b8bd81b1e0f",
+    "url": "/static/js/2.09c19b63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09c19b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a2b257c23861fe0fae6",
+    "url": "/static/js/main.1652935e.chunk.js"
+  },
+  {
+    "revision": "91d5c367e5e7f2e07b9d",
+    "url": "/static/js/runtime-main.3af5dac3.js"
+  }
+]);
